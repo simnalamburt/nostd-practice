4860cc9483ddc4c708153770fe3623e7069e412b
Jihyeon Kim <simnalamburt@gmail.com>
2025-03-25 19:46:07 +0900

    src/main.rs: Make the panic handler meaningfully

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ use syscalls::{Sysno, syscall};
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
+    let _unused = unsafe { syscall!(Sysno::exit, 1) };
+
+    #[allow(
+        clippy::empty_loop,
+        reason = "This loop is to handle the case where the `exit` syscall fails."
+    )]
     loop {}
 }
 
@@ -12,5 +18,5 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 unsafe fn _start() -> ! {
     syscall!(Sysno::write, 1, "Hello, World!\n".as_ptr(), 14).unwrap();
     syscall!(Sysno::exit, 0).unwrap();
-    loop {}
+    unreachable!();
 }
