e19ed3d2d296eccc3e581ab88ef7de6bf4abcacf
Jihyeon Kim <simnalamburt@gmail.com>
2025-03-26 00:43:05 +0900

    Test isrc

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 use core::str::from_utf8_unchecked;
 use no_std_io::io::{Cursor, Write};
 
+use isrc::Isrc;
 use lazycsv::Csv;
 use syscalls::{Sysno, syscall};
 
@@ -21,12 +22,12 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 #[unsafe(no_mangle)]
 unsafe fn _start() -> ! {
     let mut buffer = [0u8; 1024];
+    let mut writer = Cursor::new(&mut buffer[..]);
 
     let csv = Csv::new(b"a,b,c\n1,2,3\n100,200,300\n");
     for row in csv.into_rows() {
         let [r0, r1, r2] = row.unwrap();
 
-        let mut writer = Cursor::new(&mut buffer[..]);
         writeln!(
             writer,
             "{}\t{}\t{}",
@@ -35,15 +36,20 @@ unsafe fn _start() -> ! {
             unsafe { from_utf8_unchecked(r2.buf) }
         )
         .unwrap();
-        syscall!(
-            Sysno::write,
-            1,
-            writer.get_ref().as_ptr(),
-            writer.position()
-        )
-        .unwrap();
     }
+    writeln!(writer).unwrap();
 
+    let isrc = Isrc::from_code("AA6Q72000047").unwrap();
+    let code = isrc.to_code_fixed();
+    writeln!(writer, "ISRC: {}", unsafe { from_utf8_unchecked(&code) }).unwrap();
+
+    syscall!(
+        Sysno::write,
+        1,
+        writer.get_ref().as_ptr(),
+        writer.position()
+    )
+    .unwrap();
     syscall!(Sysno::exit, 0).unwrap();
     unreachable!();
 }
