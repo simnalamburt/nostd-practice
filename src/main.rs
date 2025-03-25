20337ee12748f88a7660a69de17e7739e740e833
Jihyeon Kim <simnalamburt@gmail.com>
2025-03-25 17:52:31 +0900

    Hello, world!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use syscalls::{Sysno, syscall};
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(no_mangle)]
+unsafe fn _start() -> ! {
+    syscall!(Sysno::write, 1, "Hello, World!\n".as_ptr(), 14).unwrap();
+    syscall!(Sysno::exit, 0).unwrap();
+    loop {}
 }
